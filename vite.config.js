2093aadb3dfc4386680c1581dd900e2ecd2a5cce
igor-nasciment0 <nascimentoigor1311@gmail.com>
2025-10-05 12:27:59 -0300

    alterações de porta

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()]
-})
+// https://vitejs.dev/config/
+export default ({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+  return defineConfig({
+      plugins: [react()],
+
+      server: {
+          port: parseInt(process.env.VITE_PORT),
+      },
+  });
+}
